67cae1c03da540aabf5f20d08e16987211a2a2f2
barre <Vincent.Barre@univ-lemans.fr>
2021-01-13 14:37:36 +0100

    Ajout du TP2 / Activité 1

---
+++ TP2/Activite1/params.inc.php
@@ -0,0 +1,6 @@
+<?php
+
+define ("DB_HOST", "localhost");    // Mettre "localhost:3307" pour le serveur de l'IUT
+define ("DB_NAME", "TQL_PHP");      // Mettre votre propre BD du type TQL_i20xxxx
+define ("DB_USER", "user");         // Mettre votre user IUT
+define ("DB_PASS", "pass");         // Mettre votre pass IUT
